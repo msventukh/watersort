e19ead0c457b12dfd5351396418ac744fce4a80f
msventukh <msventukh@gmail.com>
2021-02-21 23:50:39 +0100

    decouple logic from IO + split onto classes

---
+++ game/Container.py
@@ -0,0 +1,45 @@
+COLORS = {
+    '0': '\x1b[0;31;41m  \x1b[0m',
+    '1': '\x1b[0;32;42m  \x1b[0m',
+    '2': '\x1b[0;33;43m  \x1b[0m',
+    '3': '\x1b[0;34;44m  \x1b[0m',
+    '4': '\x1b[0;35;45m  \x1b[0m',
+    '5': '\x1b[0;36;46m  \x1b[0m',
+    '6': '\x1b[0;37;47m  \x1b[0m',
+    '7': '\x1b[1;31;40m[]\x1b[0m',
+    '8': '\x1b[1;34;40m[]\x1b[0m',
+    '9': '\x1b[1;33;40m[]\x1b[0m'
+}
+
+
+class Container:
+
+    @staticmethod
+    def create_empty(layers: int):
+        return Container(layers, [])
+
+    def __init__(self, size: int, blocks: list[str]):
+        self.head = '|'
+        self.blocks = blocks
+        self.size = size
+
+    def take(self):
+        return self.blocks.pop()
+
+    def put(self, value: str):
+        self.blocks.append(value)
+
+    def has_space(self):
+        return len(self.blocks) < self.size
+
+    def show_tail(self):
+        return self.blocks[len(self.blocks) - 1]
+
+    def is_empty(self):
+        return len(self.blocks) == 0
+
+    def contains_same(self):
+        return not self.has_space() and all(x == self.show_tail() for x in self.blocks)
+
+    def to_string(self):
+        return self.head + ''.join(map(lambda n: COLORS[n], self.blocks))
---
+++ game/Game.py
@@ -0,0 +1,47 @@
+import random
+from enum import Enum
+from game.Container import Container
+
+Status = Enum('GameEnding', 'victory defeat exit next impossible')
+
+
+class Game:
+    def __init__(self, colors: int, layers: int = 4):
+        while True:
+            blocks = list(str(x) for x in range(10))[:colors] * layers
+            random.shuffle(blocks)
+            self.containers = [Container(layers, blocks[x:x + layers]) for x in range(0, len(blocks), layers)] + \
+                              [Container.create_empty(layers), Container.create_empty(layers)]
+            if not self.is_defeat() and not self.is_victory():
+                break
+
+    def is_defeat(self):
+        if any(c.is_empty() for c in self.containers):
+            return False
+        tails_with_space = [c.show_tail() for c in self.containers if c.has_space()]
+        if len(tails_with_space) != len(set(tails_with_space)):
+            return False
+        tails_without_space = [c.show_tail() for c in self.containers if not c.has_space()]
+        return len(set(tails_with_space) & set(tails_without_space)) == 0
+
+    def is_victory(self):
+        return all((c.contains_same() or c.is_empty()) for c in self.containers)
+
+    def next_turn(self, source: int, dest: int):
+        if self.is_possible_move(source, dest):
+            self.containers[dest].put(self.containers[source].take())
+            if self.is_victory():
+                return Status.victory
+            if self.is_defeat():
+                return Status.defeat
+            return Status.next
+        else:
+            return Status.impossible
+
+    def is_possible_move(self, source: int, dest: int):
+        try:
+            s = self.containers[source]
+            d = self.containers[dest]
+        except IndexError:
+            return False
+        return d.has_space() and not s.is_empty() and (d.is_empty() or s.show_tail() == d.show_tail())
