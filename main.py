c44143241faa1550be0dff08afefda34270ea6ce
msventukh <msventukh@gmail.com>
2021-02-27 16:26:47 +0100

    fix validation for number layers

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def check_int_range(v: int, min_value: int, max_value: int, name: str):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--colors", help="Number of different colors [2..10]", type=check_colors, default=6)
-    parser.add_argument("-l", "--layers", help="Number of layers in a single container [2..100]", type=check_colors,
+    parser.add_argument("-l", "--layers", help="Number of layers in a single container [2..100]", type=check_layers,
                         default=4)
     args = parser.parse_args()
     start_game(args.colors, args.layers)
