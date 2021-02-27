6219fd7c6bcc71470c0402f6bedf2fd9944fc373
msventukh <msventukh@gmail.com>
2021-02-27 14:20:23 +0100

    introduce parameterization

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 from game.Container import Container
 from game.ForceQuit import ForceQuit
 from game.Game import Status, Game
@@ -59,5 +61,26 @@ def check_input(prompt: str):
             print("Please enter a number")
 
 
+def check_colors(value):
+    return check_int_range(value, 2, 10, "colors")
+
+
+def check_layers(value):
+    return check_int_range(value, 2, 100, "layers")
+
+
+def check_int_range(v: int, min_value: int, max_value: int, name: str):
+    value = int(v)
+    if value < min_value or value > max_value:
+        raise argparse.ArgumentTypeError(f"{value} is an invalid number of {name} (must be between {min_value} "
+                                         f"and {max_value})")
+    return value
+
+
 if __name__ == '__main__':
-    start_game(10, 4)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--colors", help="Number of different colors [2..10]", type=check_colors, default=6)
+    parser.add_argument("-l", "--layers", help="Number of layers in a single container [2..100]", type=check_colors,
+                        default=4)
+    args = parser.parse_args()
+    start_game(args.colors, args.layers)
